0ab0207bcbf1639613277ee346f39074f1075a2f
Owen Ni <oni@andrew.cmu.edu>
2026-08-18 22:13:33 -0700

    disable flask debug mode

--- backend/app.py
+++ backend/app.py
@@ -75,4 +75,4 @@ def search_titles():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=bool(os.environ.get("FLASK_DEBUG")))
